b578a850213d57bd4971fce3fd19b17747a0c9cf
Gary Sperka <gsperka@gmail.com>
2024-03-20 23:23:04 -0500

    rename forecast controller and remove npm google map library

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "forecast#index"
+  get "/forecast", to: "forecast#index"
 end
